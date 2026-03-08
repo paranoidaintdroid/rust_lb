67c861ed96a50dcaca5a878c1fbfb90b9da798fe
paranoidaintdroid <paranoidaintdroid@gmail.com>
2026-03-08 22:31:45 +0530

    simulating connect timeout (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use serde::Deserialize;
-use std::fs;
+use std::{
+    fs,
+    sync::{Arc, Mutex},
+};
 use thiserror::Error;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
+    select,
+    time::{Duration, sleep},
 };
+
+const IDLE_TIMEOUT: Duration = Duration::from_secs(10);
 
 #[derive(Debug, Deserialize)]
 struct Config {
@@ -29,6 +36,7 @@ fn load_config() -> Result<Config, Error> {
 
 async fn run(config: Config) -> Result<(), Error> {
     let listener = TcpListener::bind(config.listen_addr).await?;
+    let count = Arc::new(Mutex::new(0));
     loop {
         let (socket, _addr) = match listener.accept().await {
             Ok(val) => val,
@@ -38,21 +46,48 @@ async fn run(config: Config) -> Result<(), Error> {
             }
         };
 
+        let count_clone = Arc::clone(&count);
+
         tokio::spawn(async move {
-            handle_connection(socket).await;
+            let count = counted_echo(socket, count_clone).await;
+            println!("connection closed after {} chunks", count);
         });
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) {
+async fn counted_echo(mut stream: TcpStream, count_clone: Arc<Mutex<u64>>) -> u64 {
     let mut buffer = [0u8; 4096];
     loop {
-        let n = stream.read(&mut buffer).await.unwrap(); // Todo : Handle via better Error handling
-        if n == 0 {
-            break;
+        select! {
+            result = stream.read(&mut buffer) =>{
+                let n = match result {
+                    Ok(0) => break,
+                    Ok(n) => n,
+                    Err(e) =>{
+                        eprintln!("read error : {e}");
+                        break;
+                    },
+                };
+
+                {
+                    let mut count = count_clone.lock().unwrap();
+                    *count += 1;
+                }
+                if let Err(e) = stream.write_all(&buffer[..n]).await {
+                    eprintln!("write error: {e}");
+                    break;
+                } 
+            }
+
+            _ = sleep(IDLE_TIMEOUT) =>{
+                eprintln!{"Connection time out"};
+                let count = count_clone.lock().unwrap();
+                return *count;
+            }
         }
-        stream.write_all(&buffer[..n]).await.unwrap(); // Todo : Handle via better Error handling
     }
+    let count = count_clone.lock().unwrap();
+    *count
 }
 
 fn main() -> Result<(), Error> {
